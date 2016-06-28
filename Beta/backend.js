90ca3bc5b79806986b0a574dac7978ac5d7ffba6
reedyseth <reedyseth@gmail.com>
2016-06-28 16:45:47 -0600

    Update code

--- Beta/backend.js
+++ Beta/backend.js
@@ -1,20 +1,44 @@
-var app = angular.module('itemsApp',['ngRoute']);
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDgrg8MwRDdUIylykWBAQz-0twr_MO2lWE",
+	authDomain: "project-8290100803277551841.firebaseapp.com",
+	databaseURL: "https://project-8290100803277551841.firebaseio.com",
+	storageBucket: "project-8290100803277551841.appspot.com",
+};
+
+var app = angular.module('itemsApp',['ngRoute', 'firebase']);
+
+	app.service('fbRef', function(){
+		firebase.initializeApp(config);
+	});
+
+	app.service('Items', function(){
+	this.fetch = function(){
+			return [{
+					"sku"        : "001",
+					"description": "Foo Bar 1"
+				    }, {
+					"sku"        : "002",
+					"description": "Foo Bar 2"
+				    }, {
+					"sku"        : "003",
+					"description": "Foo Bar 3"
+				    }];
+	}
+});
+
 app.config(function($routeProvider){
     $routeProvider
 	.when('/', {
 	    controller:'ItemListController as itemList',
-	    templateUrl:'list.html'
+	    templateUrl:'list.html',
+	    resolve: {
+	    	items: function(Items){
+	    		return Items.fetch();
+	    	}
+	    }
 	});
 });
-app.controller('ItemListController', function() {
-    this.items = [{
-	"sku": "001",
-	"description": "Foo Bar 1"
-    }, {
-	"sku": "002",
-	"description": "Foo Bar 2"
-    }, {
-	"sku": "003",
-	"description": "Foo Bar 3"
-    }];
+app.controller('ItemListController', function(items) {
+    this.items = items;
 });
